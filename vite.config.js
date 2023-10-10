e1b2f938835b62c2300d561264f0320146f7bb20
AleksandrDukhn1 <sashkadom@gmail.com>
2023-10-10 18:56:23 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: `/test-genezis/`,
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/production-sub-path/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
